6e0d1e779c2998c425ea94c38b69957de4fd1cd6
vinushri <vinushree.testin@gmail.com>
2018-09-24 16:07:04 +0530

    Initial commit

---
+++ src/qsp/A.java
@@ -0,0 +1,26 @@
+package qsp;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import org.apache.poi.ss.usermodel.Workbook;
+
+
+
+public class A {
+	public static void main(String[] args) throws Exception
+	{
+		FileInputStream fis=new FileInputStream("./data/Book1.xlsx");
+//		Workbook w=WorkbookFactory.create(fis));
+//		String s=w.getSheet("Sheet1").getRow(0).getCell(0).toString();
+//		System.out.println(s);
+	}
+}
+		
+		
+		
+		
+		
+		
+	
+
